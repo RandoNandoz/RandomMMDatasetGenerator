a6e714e15823f67a3ffbc54b2e60115729e57bb6
Randy Zhu <30359448+RandoNandoz@users.noreply.github.com>
2019-12-04 19:43:04 -0800

    all functionality added, still buggy asf

---
+++ MMDataSetGenerator/DatasetGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MMDataSetGenerator
+{
+    public class DatasetGenerator
+    {
+        public Dictionary<string, int> MnMDictionary = new Dictionary<string, int>();
+        public void GenerateDataMnMDataset(int MnMs)
+        {
+            MnMDictionary.Clear();
+            MnMDictionary.Add("Total M&Ms", MnMs);
+            int blueMnMs = (int) (MnMs * 0.24);
+            int brownMnMs = (int) (MnMs * 0.14);
+            int greenMnMs = (int) (MnMs * 0.16);
+            int orangeMnMs = (int) (MnMs * 0.20);
+            int redMnMs = (int) (MnMs * 0.13);
+            int yellowMnMs = (int) (MnMs * 0.14);
+            MnMDictionary.Add("Blue M&Ms", blueMnMs);
+            MnMDictionary.Add("Brown M&Ms", brownMnMs);
+            MnMDictionary.Add("Green M&Ms", greenMnMs);
+            MnMDictionary.Add("Orange M&Ms", orangeMnMs);
+            MnMDictionary.Add("Red M&Ms", redMnMs);
+            MnMDictionary.Add("Yellow M&Ms", yellowMnMs);
+        }
+    }
+}
--- MMDataSetGenerator/Program.cs
+++ MMDataSetGenerator/Program.cs
@@ -1,12 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace MMDataSetGenerator
 {
-    class Program
+    static class Program
     {
+
+        private static DatasetGenerator _datasetGenerator = new DatasetGenerator();
+        private static Random _random = new Random();
         static void Main(string[] args)
         {
+            int defaultInputDeviationTolerance = 4;
+            int intuserinput;
+            int userDefinedInputDeviationTolerance = 0;
+            #region iterator
+
             
+            Console.WriteLine("How many times would you like to execute this?");
+            int iterationTimes;
+            int.TryParse(Console.ReadLine(), out iterationTimes);
+          
+            
+
+            #endregion
+
+            #region UserInput
+
+            Console.WriteLine("How many M&Ms are you going to get?");
+            intuserinput = Convert.ToInt32(GetReturnUserInput());
+            Console.WriteLine($"Standard deviation of your inputted value is {defaultInputDeviationTolerance}, " +
+                              "if you would like another value, please input it here.");
+            try
+            {
+                userDefinedInputDeviationTolerance = Convert.ToInt32(GetReturnUserInput());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("No, or malformed input detected, going with default value of 4");
+                userDefinedInputDeviationTolerance = defaultInputDeviationTolerance;
+            }
+
+            #endregion
+            #region Logic
+
+            for (int i = 0; i < iterationTimes; i++)
+            {
+                Console.WriteLine($"This is iteration {i+1}");
+                _datasetGenerator.GenerateDataMnMDataset(_random.Next(
+                    intuserinput - userDefinedInputDeviationTolerance,
+                    intuserinput + userDefinedInputDeviationTolerance));
+            
+                foreach (KeyValuePair<string,int> keyValuePair in _datasetGenerator.MnMDictionary)
+                {
+                    Console.WriteLine($"{keyValuePair.Key}, {keyValuePair.Value}");
+                }
+            }
+
+            #endregion
+
+            // Console.WriteLine(_datasetGenerator.GenerateDataMnMDataset(_random.Next(Intuserinput-= userDefinedInputDeviationTolerance, Intuserinput += userDefinedInputDeviationTolerance)));
+        }
+        private static string GetReturnUserInput()
+        {
+            string userInput;
+            try
+            {
+                userInput = Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Malformed input");
+                GetReturnUserInput();
+                throw;
+            }
+
+            return userInput;
         }
     }
+    
+
 }
