e30533453f0eb84c5531f1e8254f8f526b9f9e61
Randy Zhu <30359448+RandoNandoz@users.noreply.github.com>
2019-12-04 17:17:37 -0800

    Added README and LICENSE

--- MMDataSetGenerator/Program.cs
+++ MMDataSetGenerator/Program.cs
@@ -6,7 +6,7 @@ namespace MMDataSetGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a random dataset generator for M&Ms, this is for a science project that I was too lazy to get M&Ms or skittles I'll never eat.
